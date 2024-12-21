6454cf12e63019018b498903006cdf7086a06455
Gaurab Lohani <183300246+lohanigaurab01@users.noreply.github.com>
2024-12-21 11:39:05 +0545

    Update time.js

--- static/time.js
+++ static/time.js
@@ -1,26 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     function updateTime() {
-        // Get current date and time in Asia/Kathmandu timezone
-        let options = { timeZone: 'Asia/Kathmandu', hour12: true };
-        let formatter = new Intl.DateTimeFormat('en-US', {
-            ...options,
-            year: 'numeric',
-            month: 'short',
-            day: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric',
-        });
-        let parts = formatter.formatToParts(new Date());
-        let year = parts.find(part => part.type === 'year').value;
-        let month = parts.find(part => part.type === 'month').value;
-        let day = parts.find(part => part.type === 'day').value;
-        let hour = parts.find(part => part.type === 'hour').value;
-        let minute = parts.find(part => part.type === 'minute').value;
-        let second = parts.find(part => part.type === 'second').value;
-        let ampm = parts.find(part => part.type === 'dayPeriod').value;
+        let date = new Date();
+        let options = { 
+            timeZone: 'Asia/Kathmandu', 
+            year: 'numeric', 
+            month: 'short', 
+            day: 'numeric', 
+            hour: '2-digit', 
+            minute: '2-digit', 
+            second: '2-digit', 
+            hour12: true 
+        };
 
-        // Update the date and time display
+        let formatter = new Intl.DateTimeFormat('en-US', options);
+        let formattedDate = formatter.format(date);
+
+        let [month, day, year, time] = formattedDate.replace(',', '').split(' ');
+        let [hour, minute, second, ampm] = time.split(/[: ]/);
+
         document.querySelector('.date').innerHTML = `${month} ${day} || ${hour}:${minute}:${second} ${ampm}`;
         document.querySelector('.year').innerHTML = `${year} - Asia/Kathmandu`;
     }
