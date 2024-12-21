bcbc0df096e85ee7fae145136a1b4d7e36efd759
Gaurab Lohani <183300246+lohanigaurab01@users.noreply.github.com>
2024-12-21 11:41:56 +0545

    Update time.js

--- static/time.js
+++ static/time.js
@@ -12,11 +12,14 @@ document.addEventListener('DOMContentLoaded', function () {
             hour12: true 
         };
 
-        let formatter = new Intl.DateTimeFormat('en-US', options);
-        let formattedDate = formatter.format(date);
-
-        let [month, day, year, time] = formattedDate.replace(',', '').split(' ');
-        let [hour, minute, second, ampm] = time.split(/[: ]/);
+        let parts = new Intl.DateTimeFormat('en-US', options).formatToParts(date);
+        let year = parts.find(part => part.type === 'year').value;
+        let month = parts.find(part => part.type === 'month').value;
+        let day = parts.find(part => part.type === 'day').value;
+        let hour = parts.find(part => part.type === 'hour').value;
+        let minute = parts.find(part => part.type === 'minute').value;
+        let second = parts.find(part => part.type === 'second').value;
+        let ampm = parts.find(part => part.type === 'dayPeriod').value;
 
         document.querySelector('.date').innerHTML = `${month} ${day} || ${hour}:${minute}:${second} ${ampm}`;
         document.querySelector('.year').innerHTML = `${year} - Asia/Kathmandu`;
